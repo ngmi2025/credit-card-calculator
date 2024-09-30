964abf79a53cad89fc76fc04a427b220dcd3ef48
ngmi2025 <luke@ngmigroup.com>
2024-09-30 14:27:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,12 @@ document.addEventListener('DOMContentLoaded', function() {
             let value = this.value.replace(/[^\d]/g, '');
             this.value = value ? '$ ' + parseInt(value).toLocaleString() : '';
         });
+
+        input.addEventListener('blur', function(e) {
+            if (this.value.startsWith('$ $')) {
+                this.value = this.value.replace('$ $', '$ ');
+            }
+        });
     }
 
     formatCurrency(flightSpend);
